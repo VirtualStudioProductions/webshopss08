cd1cf9b0b6e319e96f036b281682a3edc094926a
thomas.schlaefer <thomas.schlaefer@417b9bdf-b24e-0410-b1b4-67d3c65b9859>
2008-06-06 17:20:09 +0000

    Kategorien werden nun aus der DB Tabelle ws_category ausgelesen

--- dao/DAOCategory.class.php
+++ dao/DAOCategory.class.php
@@ -10,24 +10,15 @@ class DAOCategory {
 	
 	
 	public function getAllCategories(){
-		
-		// Dummy Testarray erzeugen mit drei primitiven Kategorienamen
-		// und zurückgeben des Arrays
-		//$arr[0]["cat_name"] = "Kategorie 1";
-		//$arr[1]["cat_name"] = "Kategorie 2";
-		//$arr[2]["cat_name"] = "Kategorie 3";
-
 	
 		global $DATA_ACCESS; //Zugriff auf die DB Verbindung die in der index.php aufgebaut wird
 		
 		$sql="SELECT `cat_name` AS `name` " .
-		  "FROM " . TBL_CATEGORY . " ";
+		  "FROM " . TBL_CATEGORY . " ORDER BY `cat_name` ASC ";
 		
 		$stmt = $DATA_ACCESS->prepare($sql);
 		$stmt->execute();
-		//SQL fehler anzeigen:
-		//print_r ($stmt->errorInfo());
-		//print_r ($stmt);
+
 		array($arr);
 		$i=0;
 		//gibt schön alle db inhalte zurück
@@ -37,10 +28,7 @@ class DAOCategory {
 		$arr[$i]["name"] = $cell["name"];
 		$i=$i+1; 
 		}
-		
-		//$arr = $stmt->fetch(PDO::FETCH_ASSOC);
-		//array testweise ausgeben		
-		//print_r($arr);
+
 		return $arr;
 	
 	} // # END getAllCategories
